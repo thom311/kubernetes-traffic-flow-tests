thom311/kubernetes-traffic-flow-tests

commit a35af26e2c5c7ab39062681e8f514c443b8f691a

tft: support manifests/overrides for providing alternative manifest files
Under "manifests" directory we have various files that we render. Add a
mechanism so that the user can provide alternative files.

The user can set TFT_MANIFEST_OVERRIDES environment variable, which can
be an overrides directory. If unset, it defaults to "manifests/overrides/".
If empty, no overrides are used.

If tft looks for manifest files to render (like "pod.yaml.j2"), it now
first looks at the overrides directory. If the file exists, it is taken.
It shadows the file that we commit to git. Otherwise, tft takes the file
from "manifests/" directory, as before.

Signed-off-by: Thomas Haller <thaller@redhat.com>

tftbase.py
@@ -28,6 +28,8 @@ ENV_TFT_TEST_IMAGE_DEFAULT = (
     "ghcr.io/ovn-kubernetes/kubernetes-traffic-flow-tests:latest"
 )
 
+ENV_TFT_MANIFESTS_OVERRIDES = "TFT_MANIFESTS_OVERRIDES"
+
 
 def get_environ(name: str) -> Optional[str]:
     # Some environment variables are honored as configuration.
@@ -71,6 +73,50 @@ TFT_TESTS = "tft-tests"
 
 
 T = typing.TypeVar("T")
+
+
+cwd = os.getcwd()
+
+basedir = common.path_norm(os.path.dirname(__file__), cwd=cwd)
+
+
+def tftfile(*components: str) -> str:
+    f = basedir + "/" + "/".join(components)
+    return common.path_norm(f)
+
+
+@functools.cache
+def get_manifests_overrides() -> Optional[str]:
+    d = get_environ(ENV_TFT_MANIFESTS_OVERRIDES)
+    if d:
+        if d == "":
+            return None
+        d2 = common.path_norm(d, cwd=cwd)
+        if not os.path.isdir(d2):
+            raise ValueError(
+                "Manifest overrides directory {repr(d2)} ({ENV_TFT_TEST_IMAGE}={shlex.quote(d)}) does not exist"
+            )
+        return d2
+    return tftfile("manifests/overrides")
+
+
+@functools.cache
+def get_manifest(filename: str) -> str:
+    assert ".." not in filename.split("/")
+    overrides = get_manifests_overrides()
+    if overrides is not None:
+        f1 = common.path_norm(overrides + "/" + filename, cwd=cwd)
+        if os.path.exists(f1):
+            return f1
+    f2 = tftfile("manifests", filename)
+    if os.path.exists(f2):
+        return f2
+    msg = ""
+    if overrides is not None:
+        msg = f"{repr(f1)} and "
+    raise ValueError(
+        f"Could not find manifest file {repr(filename)}. Checked in {msg}{repr(f2)}."
+    )
 
 
 def eval_binary_opt_in(

manifests/overrides/.gitignore
@@ -1,0 +1,1 @@
+*

pluginMeasureCpu.py
@@ -52,7 +52,7 @@ class TaskMeasureCPU(PluginTask):
             tenant=tenant,
         )
 
-        self.in_file_template = "./manifests/tools-pod.yaml.j2"
+        self.in_file_template = tftbase.get_manifest("tools-pod.yaml.j2")
         self.out_file_yaml = (
             f"./manifests/yamls/tools-pod-{self.node_name}-measure-cpu.yaml"
         )

pluginMeasurePower.py
@@ -63,7 +63,7 @@ class TaskMeasurePower(PluginTask):
             tenant=tenant,
         )
 
-        self.in_file_template = "./manifests/tools-pod.yaml.j2"
+        self.in_file_template = tftbase.get_manifest("tools-pod.yaml.j2")
         self.out_file_yaml = (
             f"./manifests/yamls/tools-pod-{self.node_name}-measure-cpu.yaml"
         )

pluginValidateOffload.py
@@ -157,7 +157,7 @@ class TaskValidateOffload(PluginTask):
             tenant=tenant,
         )
 
-        self.in_file_template = "./manifests/tools-pod.yaml.j2"
+        self.in_file_template = tftbase.get_manifest("tools-pod.yaml.j2")
         self.out_file_yaml = (
             f"./manifests/yamls/tools-pod-{self.node_name}-validate-offload.yaml"
         )

task.py
@@ -479,7 +479,7 @@ class Task(ABC):
         return ip_address
 
     def create_cluster_ip_service(self) -> str:
-        in_file_template = "./manifests/svc-cluster-ip.yaml.j2"
+        in_file_template = tftbase.get_manifest("svc-cluster-ip.yaml.j2")
         out_file_yaml = "./manifests/yamls/svc-cluster-ip.yaml"
 
         self.render_file("Cluster IP Service", in_file_template, out_file_yaml)
@@ -494,7 +494,7 @@ class Task(ABC):
         ).out
 
     def create_node_port_service(self, nodeport: int) -> str:
-        in_file_template = "./manifests/svc-node-port.yaml.j2"
+        in_file_template = tftbase.get_manifest("svc-node-port.yaml.j2")
         out_file_yaml = "./manifests/yamls/svc-node-port.yaml"
 
         template_args = {
@@ -516,7 +516,7 @@ class Task(ABC):
         ).out
 
     def create_ingress_multi_network_policy(self, ingressPort: int) -> str:
-        in_file_template = "./manifests/allow-ingress-mnp.yaml.j2"
+        in_file_template = tftbase.get_manifest("allow-ingress-mnp.yaml.j2")
         out_file_yaml = "./manifests/yamls/allow-ingress-mnp.yaml"
 
         template_args = {
@@ -541,7 +541,7 @@ class Task(ABC):
         ).out
 
     def create_egress_multi_network_policy(self, egressPort: int) -> str:
-        in_file_template = "./manifests/allow-egress-mnp.yaml.j2"
+        in_file_template = tftbase.get_manifest("allow-egress-mnp.yaml.j2")
         out_file_yaml = "./manifests/yamls/allow-egress-mnp.yaml"
 
         template_args = {
@@ -741,21 +741,21 @@ class ServerTask(Task, ABC):
             ConnectionMode.MULTI_HOME,
             ConnectionMode.MULTI_NETWORK,
         ):
-            in_file_template = "./manifests/pod-secondary-network.yaml.j2"
+            in_file_template = tftbase.get_manifest("pod-secondary-network.yaml.j2")
             out_file_yaml = (
                 f"./manifests/yamls/pod-secondary-network-{node_name}-server.yaml"
             )
             pod_name = f"normal-pod-secondary-network-{node_name}-server-{port}"
         elif pod_type == PodType.SRIOV:
-            in_file_template = "./manifests/sriov-pod.yaml.j2"
+            in_file_template = tftbase.get_manifest("sriov-pod.yaml.j2")
             out_file_yaml = f"./manifests/yamls/sriov-pod-{node_name}-server.yaml"
             pod_name = f"sriov-pod-{node_name}-server-{port}"
         elif pod_type == PodType.NORMAL:
-            in_file_template = "./manifests/pod.yaml.j2"
+            in_file_template = tftbase.get_manifest("pod.yaml.j2")
             out_file_yaml = f"./manifests/yamls/pod-{node_name}-server.yaml"
             pod_name = f"normal-pod-{node_name}-server-{port}"
         elif pod_type == PodType.HOSTBACKED:
-            in_file_template = "./manifests/host-pod.yaml.j2"
+            in_file_template = tftbase.get_manifest("host-pod.yaml.j2")
             out_file_yaml = f"./manifests/yamls/host-pod-{node_name}-server.yaml"
             pod_name = f"host-pod-{node_name}-server-{port}"
         else:
@@ -898,21 +898,21 @@ class ClientTask(Task, ABC):
         connection_mode = ts.connection_mode
 
         if connection_mode in (ConnectionMode.MULTI_HOME, ConnectionMode.MULTI_NETWORK):
-            in_file_template = "./manifests/pod-secondary-network.yaml.j2"
+            in_file_template = tftbase.get_manifest("pod-secondary-network.yaml.j2")
             out_file_yaml = (
                 f"./manifests/yamls/pod-secondary-network-{node_name}-client.yaml"
             )
             pod_name = f"normal-pod-secondary-network-{node_name}-client-{port}"
         elif pod_type == PodType.SRIOV:
-            in_file_template = "./manifests/sriov-pod.yaml.j2"
+            in_file_template = tftbase.get_manifest("sriov-pod.yaml.j2")
             out_file_yaml = f"./manifests/yamls/sriov-pod-{node_name}-client.yaml"
             pod_name = f"sriov-pod-{node_name}-client-{port}"
         elif pod_type == PodType.NORMAL:
-            in_file_template = "./manifests/pod.yaml.j2"
+            in_file_template = tftbase.get_manifest("pod.yaml.j2")
             out_file_yaml = f"./manifests/yamls/pod-{node_name}-client.yaml"
             pod_name = f"normal-pod-{node_name}-client-{port}"
         elif pod_type == PodType.HOSTBACKED:
-            in_file_template = "./manifests/host-pod.yaml.j2"
+            in_file_template = tftbase.get_manifest("host-pod.yaml.j2")
             out_file_yaml = f"./manifests/yamls/host-pod-{node_name}-client.yaml"
             pod_name = f"host-pod-{node_name}-client-{port}"
         else:

tests/test_tftbase.py
@@ -165,3 +165,23 @@ def test_eval_binary_opt_in() -> None:
 
     assert tftbase.eval_binary_opt_in(True, False) == (True, False)
     assert tftbase.eval_binary_opt_in(False, True) == (False, True)
+
+
+def test_tftfile() -> None:
+    f = tftbase.tftfile("manifests/host-pod.yaml.j2")
+    assert f == tftbase.tftfile("manifests", "host-pod.yaml.j2")
+    assert f == tftbase.tftfile("manifests", "./host-pod.yaml.j2")
+    assert os.path.exists(f)
+    assert f.endswith("/manifests/host-pod.yaml.j2")
+
+
+def test_get_manifest() -> None:
+    if os.getenv(tftbase.ENV_TFT_MANIFESTS_OVERRIDES):
+        return
+
+    f = tftbase.get_manifest("host-pod.yaml.j2")
+    assert f in (
+        tftbase.tftfile("manifests/host-pod.yaml.j2"),
+        tftbase.tftfile("manifests/overrides/host-pod.yaml.j2"),
+    )
+    assert os.path.exists(f)

commit 2e880f7fa2c6a11955ef028901537227d0191cae

tftbase: fix handling empty TFT_MANIFESTS_OVERRIDES variable
Fixes: a35af26e2c5c ('tft: support manifests/overrides for providing alternative manifest files')
Signed-off-by: Thomas Haller <thaller@redhat.com>

tftbase.py
@@ -89,14 +89,14 @@ def tftfile(*components: str) -> str:
 def get_manifests_overrides() -> Optional[str]:
     d = get_environ(ENV_TFT_MANIFESTS_OVERRIDES)
     if d:
-        if d == "":
-            return None
         d2 = common.path_norm(d, cwd=cwd)
         if not os.path.isdir(d2):
             raise ValueError(
-                "Manifest overrides directory {repr(d2)} ({ENV_TFT_TEST_IMAGE}={shlex.quote(d)}) does not exist"
+                "Manifest overrides directory {repr(d2)} ({ENV_TFT_MANIFESTS_OVERRIDES}={shlex.quote(d)}) does not exist"
             )
         return d2
+    if d == "":
+        return None
     return tftfile("manifests/overrides")
 
 